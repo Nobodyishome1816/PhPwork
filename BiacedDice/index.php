035c93203eee7bf5984a69dda28117a481df8358
tommcallister41 <tommcallister1816@gmail.com>
2024-09-19 10:09:19 +0100

    Initial commit

---
+++ BiacedDice/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<HTML>
+    <HEADL>
+        <link rel="stylesheet" href="styles.css">
+        <title>Biased Dice Roller</title>
+    </HEADL>
+    <BODY>
+        <h1>Biased Dice Roller </h1>
+    <?PHP
+    ?>
+    </BODY>
+</HTML>
